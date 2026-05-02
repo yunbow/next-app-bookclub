cc9746d28d7af669d68b16c291a09f5c10945a4b
yunbow <yunbow365@gmail.com>
2026-05-02 17:06:31 +0900

    feat(infra): TICKET-0001 PostgreSQL docker-compose追加 / prisma schema PostgreSQL化 / migrations削除

--- prisma/migrations/20260305150206_add_remaining_features/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "books" ADD COLUMN "pages" INTEGER;
-
--- AlterTable
-ALTER TABLE "reviews" ADD COLUMN "images" TEXT;
-
--- AlterTable
-ALTER TABLE "users" ADD COLUMN "notificationSettings" TEXT;
